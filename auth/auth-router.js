64d2f853ce253e0937f4b1357b3fe44efbac4b6b
josiah <josiahdamiwilliams@gmail.com>
2020-03-23 16:07:13 +0100

    Installed additional dependencies, created functions for register, lo…
    …gin and logout

---
+++ auth/auth-router.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const db = require('./user-route-helper')
+const bcrypt = require('bcryptjs')
+
+
+
+router.post("/register", (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+
+	Users.add({ username: req.body.username, password: req.body.password })
+		.then(saved => {
+			res.status(201).json(saved);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: `Something went really poorly` })
+          })
+        })
+
+        router.post("/login", (req, res) => {
+            const { username, password } = req.body;
+        
+                db.findBy({ username })
+                    .first()
+                    .then( user => {
+                        if (user && bcrypt.compareSync(password, user.password)) {
+                            req.session.user = user.username;
+                            console.log(req.session);
+                            res.status(200).json({ message: `Welcome, on board ${user.username}`})
+                        } else {
+                            res.status(401).json({ message: "Invalid Credential"})
+                        }
+                    })
+                    .catch(error => {
+                        res.status(500).json({ message: "Oops. Technical difficulties on our part " + error.message})
+                    })        
+        
+        
+        })
+        
+        function protected (req, res, next)  {
+            console.log(req.session)
+            if (req.session && req.session.user) {
+                next();
+            } else {
+                res.status(401).json({ message: "Invalid credentials."})
+            }
+    }
+
+    router.get("/users", protected, (req, res) => {
+        db.retrieve()
+            .then(users => {
+                res.status(200).json(users)
+            })
+            .catch(error => {
+                res.status(500).json({ message: "Oops!, Something went wrong. " + error.message})
+            })
+    })
+    
+    router.get("/logout", (req, res) => {
+        if (req.session) {
+            req.session.destroy();
+            res.send("See you soon");
+        } else {
+            res.status(401).json({ message: "Not logged in"})
+        }
+    
+    })
--- server.js
+++ server.js
@@ -5,8 +5,30 @@ const cors = require('cors');
 const Users = require('./users/user-route');
 
 const server = express();
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+// const knexConfig = require("./database/db-config");
 
+
+const sessionConfiguration = {
+    name: "hai",
+    secret: "not for you",
+    cookie: {
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60,
+        secure: false
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new KnexSessionStore({
+        knex: knexConfig,
+        createtable: true,
+        clearInterval: 1000 * 60 * 30
+  
+    })
+  }
 server.use(helmet());
+server.use(session(sessionConfiguration));
 server.use(express.json());
 server.use(cors());
 server.use('/api', Users)
